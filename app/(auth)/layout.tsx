8f0a79512ae6fb48491b19a8c08ae99b2334630a
Tushar Gaurav <tushar.gaurav416@gmail.com>
2024-06-23 10:11:30 +0530

    fix: auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import {
     Button,
     Checkbox,
@@ -12,24 +10,37 @@ import {
     Stack,
     Image,
 } from '@chakra-ui/react'
+import Link from 'next/link';
+
+const helloGreetingsInDiffrentLang = ["Hello", "Bonjour", "Hola", "Ciao", "Hallo", "Olá", "Namaste", "Konnichiwa", "Ni Hao", "Salam", "Merhaba", "Sawubona", "Jambo", "Zdravstvuyte", "Aloha", "Shalom", "Guten Tag", "Dzień dobry", "Kalimera", "Szia", "Dobrý den", "God dag", "Hyvää päivää", "God dag", "Dobrý deň", "Dobrý den", "Dzień dobry", "Kalimera", "Szia", "Guten Tag", "Shalom", "Aloha", "Zdravstvuyte", "Jambo", "Sawubona", "Merhaba", "Salam", "Ni Hao", "Konnichiwa", "Namaste", "Olá", "Hallo", "Ciao", "Hola", "Bonjour", "Hello"]
 
 export default function AuthLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const randomIndex = Math.floor(Math.random() * helloGreetingsInDiffrentLang.length)
+
     return (
-        <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-            <Flex p={8} flex={1} align={'center'} justify={'center'}>
+        <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }} className='bg-primaryblue'>
+
+            {/* Place the logo to the top right of the page */}
+            <Link href='/'>
+                <Image src="/logo_bg.png" width={120} alt="Logo" className='absolute top-4 left-4' />
+            </Link>
+
+            <Flex p={8} flex={1} align={'center'} justify={'center'} className='flex-col'>
+                <h1 className='text-6xl mt-16 md:mt-0' style={{ color: "#D7DEFD" }}>{helloGreetingsInDiffrentLang[randomIndex]},</h1>
+                <h1 className='text-5xl font-black text-white pb-4'>welcome!</h1>
+
                 {children}
             </Flex>
             <Flex flex={1}>
                 <Image
                     alt={'Login Image'}
                     objectFit={'cover'}
-                    src={
-                        'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
-                    }
+                    src="/auth_page.webp"
+                    className='hidden md:block w-full h-full object-cover'
                 />
             </Flex>
         </Stack>
--- app/(auth)/register/[[...register]]/page.tsx
+++ app/(auth)/register/[[...register]]/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import { SignUp } from "@clerk/nextjs";
+
+export const metadata: Metadata = {
+    title: 'Sign In - SpeedyUploads',
+    description: 'Sign in to your SpeedyUploads account',
+}
 
 export default function Page() {
     return <SignUp />;
